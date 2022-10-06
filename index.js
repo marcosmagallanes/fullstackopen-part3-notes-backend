408455bd2c7ef303f296c9df0a7da7844e4eb2c5
Marcos <marcos.m.magallanes@gmail.com>
2022-10-05 20:26:29 -0600

    working state

--- index.js
+++ index.js
@@ -1,82 +1,119 @@
 const express = require('express')
 const app = express()
+const nanoid = require('nanoid')
+const morgan = require('morgan')
 
-let notes = [
+app.use(express.json());
+
+morgan.token('data', (req, res) => {
+  const { body } = req
+  return JSON.stringify(body)
+})
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+
+let phonebook = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+app.get('/info', (req, res) => {
+  res.json({
+    num_of_entries: phonebook.length,
+    timestamp: Date()
+  })
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(phonebook)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.get("/api/persons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
 
-  response.status(204).end()
-})
+  const filteredPhonebook = phonebook.filter(item => item.id === id)
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
+  if (filteredPhonebook.length === 1) {
+    res.json(...filteredPhonebook)
   } else {
-    response.status(404).end()
+    res.json({ error: 'no entry found by that id' })
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  let returnObj;
+
+  const filteredPhonebook = phonebook.filter((item) => {
+    if (item.id === id) {
+      returnObj = item
+      return false
+    } else return true
+  });
+
+  if (returnObj) res.json({
+    deleted: returnObj,
+  })
+  else res.json({ error: 'no entry found by that id to delete'})
+})
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body
+
+  if (!name || !number) {
+    res.json({ error: 'name or number is missing'})
+  } else {
+
+    const nameExists = phonebook.map(entry => entry.name).includes(name)
+
+    console.log(phonebook.map((entry) => entry.name));
+    console.log(nameExists)    
+
+    if (nameExists) {
+      res.json({ error: "existing entry found by that name" }); 
+    } else {
+      const newEntry = {
+        name,
+        number,
+        id: nanoid(6),
+      };
+      phonebook = phonebook.concat(newEntry);
+      res.json(newEntry);
+    }
   }
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001
 app.listen(PORT, () => {
